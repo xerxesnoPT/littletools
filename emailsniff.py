ef1c8718b1bf161d555254d5abf15a5019b00bd6
xerxes <346287244@qq.com>
2018-02-22 16:19:20 +0800

    add copy text to system function in tmux.conf

--- emailsniff.py
+++ emailsniff.py
@@ -1,10 +1,12 @@
 from scapy.all import *
 def packet_callback(packet):
     if packet[TCP].payload:
-        mail_packet = packet[TCP].payload
-        print(mail_packet)
-        #  if "user" in mail_packet.lower() or "pass" in mail_packet.lower():
-            #  print ("[*] Server : %s" % packet[IP].dst)
-            #  print ("[*]  %s" % packet[TCP].payload)
+        mail_packet = bytes(packet[TCP].payload)
+        print("start!!")
+        #  if "user" in str(mail_packet).lower() or "pass" in str(mail_packet).lower():
+        print ("[*] Server : %s" % packet[IP].dst)
+        #  b = bytes(packet[TCP].payload)
+        #  print ("[*]  %s" % b.decode('ascii',errors='ignore'))
+        print ("[*]  %s" % packet[TCP].payload)
 
 sniff(filter='tcp port 110 or tcp port 25 or tcp port 143',prn=packet_callback)
